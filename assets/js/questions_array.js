a68064495d71c963fa0c2518366351502f672c57
emmahewson <emhewson@hotmail.com>
2023-01-25 14:56:43 +0000

    add questions array

---
+++ assets/js/questions_array.js
@@ -0,0 +1,351 @@
+const questions = [{
+    questionNumber: 1,
+    questionText: "At a party you are most likely to be found…",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Leafing through the host's book collection",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Jumping off the roof in to the pool",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Hanging out with the pet dog",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Telling a funny story to a crowd of people",
+            answerType: "people"
+        },
+        {
+            answerNumber: 5,
+            answerText: "In the kitchen making 'helpful' cooking suggestions",
+            answerType: "food"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Lying in the garden looking at the stars",
+            answerType: "remote"
+        },
+    ]
+},
+{
+    questionNumber: 2,
+    questionText: "Your ideal weekend would involve…",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Alpaca walking",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 2,
+            answerText: "A day shopping in a big city",
+            answerType: "people"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Wandering around a museum",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Getting as far away from other human beings as possible",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 5,
+            answerText: "Mountain biking down Snowdon",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Trying out a new restaurant",
+            answerType: "food"
+        },
+    ]
+},
+{
+    questionNumber: 3,
+    questionText: "If you could only listen to one type of music for the rest of your life you would choose…",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Punk",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Silence",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Pop",
+            answerType: "people"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Jazz",
+            answerType: "food"
+        },
+        {
+            answerNumber: 5,
+            answerText: "Whalesong",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Classical",
+            answerType: "culture"
+        },
+    ]
+},
+{
+    questionNumber: 4,
+    questionText: "Which of these sounds like your kind of breakfast?",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Sprouted millet, sorghum and buckwheat toast with avocado and cultured vegetables",
+            answerType: "food"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Coffee at an art gallery cafe",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Getting up at 4am to drive to the beach to go surfing, and grabbing whatever you can find at a service station",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Anything as long as you can share it with your dog",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 5,
+            answerText: "Toast cooked on a fork over an open fire",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Brunch out in a busy cafe with good friends",
+            answerType: "people"
+        },
+    ]
+},
+{
+    questionNumber: 5,
+    questionText: "Which part of the internet are you most likely to waste your life on?",
+    answers: [{
+            answerNumber: 1,
+            answerText: "I try to avoid places with phone signal",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Anywhere with funny cat videos",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 3,
+            answerText: "BBC good food",
+            answerType: "food"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Nowhere, I accidentally dropped my phone in to a ravine",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 5,
+            answerText: "Twitter",
+            answerType: "people"
+        },
+        {
+            answerNumber: 6,
+            answerText: "The Guardian Website",
+            answerType: "culture"
+        },
+    ]
+},
+{
+    questionNumber: 6,
+    questionText: "Which of these people is your favourite?",
+    answers: [{
+            answerNumber: 1,
+            answerText: "David Attenborough",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Graham Norton",
+            answerType: "people"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Ranulph Fiennes",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Stephen Fry",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 5,
+            answerText: "Nigella Lawson",
+            answerType: "food"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Ken Block",
+            answerType: "thrill"
+        },
+    ]
+},
+{
+    questionNumber: 7,
+    questionText: "What's your favourite breed of dog?",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Golden Retriever",
+            answerType: "people"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Labrador",
+            answerType: "food"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Border Collie",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Huskie",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 5,
+            answerText: "King Charles Spaniel",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Rescue Dog",
+            answerType: "wildlife"
+        },
+    ]
+},
+{
+    questionNumber: 8,
+    questionText: "What was your favourite subject at school?",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Geography",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 2,
+            answerText: "History",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Psychology",
+            answerType: "people"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Food Tech",
+            answerType: "food"
+        },
+        {
+            answerNumber: 5,
+            answerText: "Biology",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Physical Education",
+            answerType: "thrill"
+        },
+    ]
+},
+{
+    questionNumber: 9,
+    questionText: "Which of the following would be your most likely excuse for being late for work…",
+    answers: [{
+            answerNumber: 1,
+            answerText: "I was googling the history of the ancient Egyptians and I ended up drowning in an infinite internet wormhole of facts.",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 2,
+            answerText: "My best friend called to tell me about the date they had last night.",
+            answerType: "people"
+        },
+        {
+            answerNumber: 3,
+            answerText: "I saw an upside-down sheep by the side of the road and I stopped to turn it over.",
+            answerType: "wildlife"
+        },
+        {
+            answerNumber: 4,
+            answerText: "I live 300 miles away in a valley populated only by myself and a pack of wolves.",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 5,
+            answerText: "I got stuck in a glacial cravasse and had to drag myself out using only my teeth.",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 6,
+            answerText: "I had to wait until my toast was exactly the right temperature before I added the 2nd layer of artisan butter.",
+            answerType: "food"
+        },
+    ]
+},
+{
+    questionNumber: 10,
+    questionText: "What would be your ideal superpower?",
+    answers: [{
+            answerNumber: 1,
+            answerText: "Flight",
+            answerType: "thrill"
+        },
+        {
+            answerNumber: 2,
+            answerText: "Never putting on weight no matter how much I eat",
+            answerType: "food"
+        },
+        {
+            answerNumber: 3,
+            answerText: "Infinite knowledge",
+            answerType: "culture"
+        },
+        {
+            answerNumber: 4,
+            answerText: "Mind reading",
+            answerType: "people"
+        },
+        {
+            answerNumber: 5,
+            answerText: "The ability to control fire",
+            answerType: "remote"
+        },
+        {
+            answerNumber: 6,
+            answerText: "Talking to animals",
+            answerType: "wildlife"
+        },
+    ]
+},
+]
