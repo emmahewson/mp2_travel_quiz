d1c62da1f13aaadc08d365887d9a1954a9024f7d
emmahewson <emhewson@hotmail.com>
2023-01-25 15:08:35 +0000

    added for loop to make answer function more concise

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,15 +1,17 @@
 
 
-let startGameBtn = document.getElementById("start-game")
-let nextQuestionBtn = document.getElementById("next-question-btn")
-let gameDiv = document.getElementById("game-div")
-let questionText = document.getElementById("question-text")
-let btnOne = document.getElementById("btn1");
-let btnTwo = document.getElementById("btn2");
-let btnThree = document.getElementById("btn3");
-let btnFour = document.getElementById("btn4");
-let btnFive = document.getElementById("btn5");
-let btnSix = document.getElementById("btn6");
+const startGameBtn = document.getElementById("start-game")
+const nextQuestionBtn = document.getElementById("next-question-btn")
+const gameDiv = document.getElementById("game-div")
+const questionText = document.getElementById("question-text")
+const choices = Array.from(document.getElementsByClassName("choice"))
+
+const btnOne = document.getElementById("btn1");
+const btnTwo = document.getElementById("btn2");
+const btnThree = document.getElementById("btn3");
+const btnFour = document.getElementById("btn4");
+const btnFive = document.getElementById("btn5");
+const btnSix = document.getElementById("btn6");
 
 
 startGameBtn.addEventListener('click', function startGame() {
@@ -18,25 +20,23 @@ startGameBtn.addEventListener('click', function startGame() {
     addQuestionContent(0);
 
 })
-
+function populateAnswers(index) {
+    let answers = questions[index].answers;
+    for (let i = 0; i < answers.length; i++) {
+        choices[i].innerText = answers[i].answerText;
+    }
+}
 
 function addQuestionContent(index) {
     questionText.innerText = questions[0].questionText;
-    let answers = questions[index].answers;
-
-    btnOne.innerText = answers[0].answerText;
-    btnTwo.innerText = answers[1].answerText;
-    btnThree.innerText = answers[2].answerText;
-    btnFour.innerText = answers[3].answerText;
-    btnFive.innerText = answers[4].answerText;
-    btnSix.innerText = answers[5].answerText;
-};
+    populateAnswers(index)
+        choices
+    }
 
 nextQuestionBtn.addEventListener('click', function nextQuestion() {
     questions.splice(0, 1);
     addQuestionContent(0);
 })
-
 
 
 
