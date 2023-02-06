1614cb531a4b155d10ff7c6f032c9134c2dbae29
emmahewson <emhewson@hotmail.com>
2023-02-06 16:37:51 +0000

    add google maps API with clickable markers

--- assets/js/countries_array.js
+++ assets/js/countries_array.js
@@ -1,13 +1,84 @@
-const countries = [
-    {
+const countries = [{
         name: "New Zealand",
         image: "newzealand_flag.png",
-        text: "New Zealand is a country with lots of exciting things to see and do!"
+        text: "New Zealand is a country with lots of exciting things to see and do!",
+        zoom: 5,
+        center: {
+            lat: -41.26033597615464,
+            lng: 173.27204569162367
+        },
+        highlights: [{
+                title: 'Queenstown',
+                lat: '-44.471390897973976',
+                lng: '168.585126556376',
+                description: 'Text describing Queenstown',
+                imgOne: "newzealand_flag.png",
+                imgTwo: "mexico_flag.png",
+                imgThree: "peru_flag.png",
+                imgFour: "china_flag.png"
+            },
+            {
+                title: 'Christchurch',
+                lat: '-43.35326145225949',
+                lng: '172.72422397451683',
+                description: 'Text describing Christchurch',
+                imgOne: "china_flag.png",
+                imgTwo: "peru_flag.png",
+                imgThree: "mexico_flag.png",
+                imgFour: "newzealand_flag.png"
+            },
+            {
+                title: 'Auckland',
+                lat: '-36.672487924689015',
+                lng: '174.70246906406936',
+                description: 'Text describing Auckland',
+                imgOne: "mexico_flag.png",
+                imgTwo: "newzealand_flag.png",
+                imgThree: "china_flag.png",
+                imgFour: "peru_flag.png"
+            }
+        ]
     },
     {
         name: "Mexico",
         image: "mexico_flag.png",
-        text: "Mexico is a country with lots of exciting things to see and do!"
+        text: "Mexico is a country with lots of exciting things to see and do!",
+        zoom: 5,
+        center: {
+            lat: 22.52994095041273,
+            lng: -102.20123143246553
+        },
+        highlights: [{
+                title: 'Mexico 1',
+                lat: '23.62150535056615',
+                lng: '-106.15630963882359',
+                description: 'Text describing Queenstown',
+                imgOne: "newzealand_flag.png",
+                imgTwo: "mexico_flag.png",
+                imgThree: "peru_flag.png",
+                imgFour: "china_flag.png"
+            },
+            {
+                title: 'Mexico 2',
+                lat: '17.703824138142227',
+                lng: '-93.93951226121168',
+                description: 'Text describing Christchurch',
+                imgOne: "china_flag.png",
+                imgTwo: "peru_flag.png",
+                imgThree: "mexico_flag.png",
+                imgFour: "newzealand_flag.png"
+            },
+            {
+                title: 'Mexico 3',
+                lat: '26.40872613841027',
+                lng: '-100.53130940740515',
+                description: 'Text describing Auckland',
+                imgOne: "mexico_flag.png",
+                imgTwo: "newzealand_flag.png",
+                imgThree: "china_flag.png",
+                imgFour: "peru_flag.png"
+            }
+        ]
     },
     {
         name: "Peru",
---
+++ assets/js/map.js
@@ -0,0 +1,50 @@
+
+// REPLACE ONCE REST OF QUIZ IS BUILT - CURRENTLY APPEARS ONLOAD
+    window.onload = function () {
+        initMap(1);
+    }
+    
+    function initMap(index) {
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: countries[index].zoom,
+            center: countries[index].center
+        })
+ 
+        //Declaring DOM elements
+        const highlightTitle = document.getElementById('highlight-title')
+        const highlightText = document.getElementById('highlight-text')
+        const imageOne = document.getElementById('highlight-photo-1')
+        const imageTwo = document.getElementById('highlight-photo-2')
+        const imageThree = document.getElementById('highlight-photo-3')
+        const imageFour = document.getElementById('highlight-photo-4')
+ 
+        // Clickable map markers adapted from: https://www.aspsnippets.com/Articles/Google-Maps-API-V3-Add-click-event-listener-to-all-multiple-markers.aspx
+
+        // Populating the Map with Markers & attaching text & photos to each marker ready to be called on click
+        for (let i = 0; i < countries[index].highlights.length; i++) {
+            let data = countries[index].highlights[i];
+            let myLatlng = new google.maps.LatLng(data.lat, data.lng);
+            let marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: data.title,
+                text: data.description,
+                imgOne: data.imgOne,
+                imgTwo: data.imgTwo,
+                imgThree: data.imgThree,
+                imgFour: data.imgFour
+            });
+ 
+            //Attach click event to the marker & populate page with data
+            (function (marker, data) {
+                google.maps.event.addListener(marker, "click", function (e) {
+                    highlightTitle.innerText = this.title;
+                    highlightText.innerText = this.text;
+                    imageOne.src = `assets/images/${this.imgOne}`;
+                    imageTwo.src = `assets/images/${this.imgTwo}`;
+                    imageThree.src = `assets/images/${this.imgThree}`;
+                    imageFour.src = `assets/images/${this.imgFour}`;
+                });
+            })(marker, data);
+        }
+    }
