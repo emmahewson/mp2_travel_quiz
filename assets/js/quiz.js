b5abfff9106502b46d1e9d1a7099879df32215df
emmahewson <emhewson@hotmail.com>
2023-03-13 12:02:48 +0000

    tidy css & js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -365,7 +365,6 @@ function startGame(event) {
             percentageArray[0] += percentDifference;
         }
         return percentageArray;
-
     }
 
     // helper function for calculatePiePersonalities - calculate percentage
